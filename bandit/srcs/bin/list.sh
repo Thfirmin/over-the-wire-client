ecd56bcf6c28c5bfbccb1b3816bcd1019519d497
Thfirmin <thfirmin.dsantos+github@gmail.com>
2025-10-28 01:06:13 -0300

    hotfix: bandit list wrong tags

--- bandit/srcs/bin/list.sh
+++ bandit/srcs/bin/list.sh
@@ -3,16 +3,16 @@
 BANDIT_LVL=$(ls -d bandit*[0-9] | sed "s/^bandit//g")
 
 for lvl in $BANDIT_LVL; do
-	CONCLUDED=$(ls "bandit${lvl}/data.txt" 2>/dev/null)
+	HASPSSWD=$(ls "bandit${lvl}/data.txt" 2>/dev/null)
 	SOLVED=$(ls "bandit${lvl}/solve.sh" 2>/dev/null)
 
-	if [ -n "${CONCLUDED}" ]; then
-		echo -e "\x1b[32m[ lvl $lvl ]: ABLE CONNECT\x1b[0m"
+	if [ -n "${SOLVED}" ]; then
+		echo -e "\x1b[32m[ lvl $lvl ]: SOLVED\x1b[0m"
 	else
-		if [ -n "${SOLVED}" ]; then
-			echo -e "\x1b[33m[ lvl $lvl ]: SOLVED\x1b[0m"
+		if [ -n "${HASPSSWD}" ]; then
+			echo -e "\x1b[33m[ lvl $lvl ]: ABLE TO START\x1b[0m"
 		else
-			echo -e "\x1b[31m[ lvl $lvl ]: NOT SOLVED YET\x1b[0m"
+			echo -e "\x1b[31m[ lvl $lvl ]: NOT STARTED YET\x1b[0m"
 		fi
 	fi
 done
